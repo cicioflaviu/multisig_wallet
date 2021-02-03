b65a1408a6732701b619c438d6e23155b3e9d3b4
Flaviu Cicio <cicioflaviu@gmail.com>
2021-02-03 19:41:50 +0200

    Implement client

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -37,7 +37,7 @@ contract Wallet {
         ));
     }
     
-    function approveTransfer(uint id) external onlyApprover {
+    function approveTransfer(uint id) external onlyApprover payable {
         require(transfers[id].sent == false, 'transfer has already be sent');
         require(approvals[msg.sender][id] == false, 'cannot approve transfer twice');
         
